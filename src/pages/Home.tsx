8439aa8cd024732f9bfd0c7cc40de7a3d84e6fe2
Harsh Rohila <rohilaharsh@gmail.com>
2020-04-12 22:21:43 +0530

    added fab button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,31 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonPage } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { IonFab, IonFabButton, IonIcon, IonFabList, IonText } from '@ionic/react';
+import { add } from 'ionicons/icons'; 
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <ExploreContainer />
+
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton>
+            <IonIcon icon={add} />
+          </IonFabButton>
+
+          <IonFabList side="top">
+            <IonFabButton>
+              <IonText color="primary">
+                <p>Break</p>
+              </IonText>
+            </IonFabButton>
+          </IonFabList>
+
+        </IonFab>
+
       </IonContent>
     </IonPage>
   );
