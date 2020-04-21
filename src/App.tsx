ca85a5f8d7abed2332dc6be2558a98df8948f01d
Harsh Rohila <harsh_rohila@trimble.com>
2020-04-21 10:29:15 +0530

    using home component

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route path="/home" component={TimeTable} exact={true} />
+        <Route path="/home" component={Home} exact={true} />
         <Route exact path="/" render={() => <Redirect to="/home" />} />
       </IonRouterOutlet>
     </IonReactRouter>
